a3f8d861fe879b40cce9f30b0aa40f5c70893a21
Bartek <bartek.pozniewski@wp.pl>
2022-10-07 19:18:59 +0200

    First commit

---
+++ src/main/java/com/example/ex1/Question.java
@@ -0,0 +1,12 @@
+package com.example.ex1;
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId,boolean trueAnswer)
+    {
+        this.questionId=questionId;
+        this.trueAnswer=trueAnswer;
+    }
+}
